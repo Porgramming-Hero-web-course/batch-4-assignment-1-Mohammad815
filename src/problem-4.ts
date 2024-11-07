9903ddbdbf275258ff3150433f01424c723150c7
Mohammad Forhad <mohammadforhad176@gmail.com>
2024-11-07 20:45:37 +0600

    Problem 4 is completed

---
+++ src/problem-4.ts
@@ -0,0 +1,36 @@
+// problem 4
+type Circle = {
+    shape : 'circle',
+    radius : number
+}
+type Rectangle  = {
+    shape: 'rectangle',
+    width: number,
+    height: number
+}
+type Shape = Circle | Rectangle;
+
+
+
+
+function calculateShapeArea(value:Shape){
+    if(value.shape === 'circle'){
+        return Math.PI * value.radius * value.radius
+    }
+    if(value.shape === 'rectangle'){
+        return value.height * value.width;
+    }
+}
+
+const circle:Circle = { shape: "circle", radius: 5 }
+const rectangle:Rectangle = {
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+  }
+
+const circleArea = calculateShapeArea(circle);
+console.log(circleArea)
+
+const rectangleArea = calculateShapeArea(rectangle);
+console.log(rectangleArea)
